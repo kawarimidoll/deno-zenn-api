2ba860e57b63a44a30cac20f9bfffb90d088d69b
kawarimidoll <kawarimidoll+git@gmail.com>
2021-06-25 18:16:39 +0900

    add mod.ts

---
+++ mod.ts
@@ -0,0 +1,19 @@
+export { ZENN_ROOT, zennApi } from "./zenn_api.ts";
+
+export { zennLink } from "./zenn_link.ts";
+
+export type {
+  ZennArticle,
+  ZennBook,
+  ZennScrap,
+  ZennTopic,
+  ZennUser,
+} from "./types.ts";
+
+export {
+  implementsZennArticle,
+  implementsZennBook,
+  implementsZennScrap,
+  implementsZennTopic,
+  implementsZennUser,
+} from "./types.ts";
