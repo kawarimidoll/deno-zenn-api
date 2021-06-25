d5a65351ab0e9fd59ca417041aece8948a56ecd5
kawarimidoll <kawarimidoll+git@gmail.com>
2021-06-25 15:47:22 +0900

    add zenn_link.ts

---
+++ zenn_link.ts
@@ -0,0 +1,36 @@
+import { ZENN_ROOT } from "./zenn_api.ts";
+import {
+  implementsZennArticle,
+  implementsZennBook,
+  implementsZennTopic,
+  implementsZennUser,
+  ZennArticle,
+  ZennBook,
+  ZennScrap,
+  ZennTopic,
+  ZennUser,
+} from "./types.ts";
+
+const zennLink = (
+  object: ZennArticle | ZennBook | ZennScrap | ZennUser | ZennTopic,
+): string => {
+  if (implementsZennUser(object)) {
+    return ZENN_ROOT + object.username;
+  }
+
+  if (implementsZennTopic(object)) {
+    return ZENN_ROOT + object.name;
+  }
+
+  const username = object.user.username;
+
+  const resourceName = implementsZennArticle(object)
+    ? "/articles/"
+    : implementsZennBook(object)
+    ? "/book/"
+    : "/scrap/";
+
+  return ZENN_ROOT + username + resourceName + object.slug;
+};
+
+export { zennLink };
