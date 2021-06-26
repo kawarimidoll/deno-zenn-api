5130deac678d689fd286b0c8337a1a4f63c6d53a
kawarimidoll <kawarimidoll+git@gmail.com>
2021-06-26 22:25:24 +0900

    revert to use std/http

--- api/deps.ts
+++ api/deps.ts
@@ -1,7 +1,12 @@
 import ky, { SearchParamsOption } from "https://cdn.skypack.dev/ky?dts";
 import { DOMParser } from "https://deno.land/x/deno_dom@v0.1.12-alpha/deno-dom-wasm.ts";
 
+import {
+  serve,
+  ServerRequest,
+} from "https://deno.land/std@0.99.0/http/server.ts";
+
 import { assert } from "https://deno.land/std@0.99.0/testing/asserts.ts";
 
-export { assert, DOMParser, ky };
+export { assert, DOMParser, ky, serve, ServerRequest };
 export type { SearchParamsOption };
--- debug.ts
+++ debug.ts
@@ -1,15 +1,10 @@
+import { serve } from "./api/deps.ts";
 import handler from "./api/server.ts";
 
 const port = 8080;
-const server = Deno.listen({ port });
+const server = serve({ port });
 console.log(`HTTP webserver running. Access it at: http://localhost:${port}/`);
 
 for await (const request of server) {
-  (async () => {
-    const httpCon = Deno.serveHttp(request);
-
-    for await (const event of httpCon) {
-      handler(event);
-    }
-  })();
+  handler(request);
 }
