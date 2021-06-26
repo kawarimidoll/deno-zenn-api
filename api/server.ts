10aac2e4b1c8484ede2ced54653d524a964b69b4
kawarimidoll <kawarimidoll+git@gmail.com>
2021-06-26 17:46:12 +0900

    add cache-control

--- api/server.ts
+++ api/server.ts
@@ -1,9 +1,14 @@
 import { ServerRequest } from "./deps.ts";
 import { zennApi } from "./zenn_api.ts";
 
+// cache one hour
+const CACHE_MAX_AGE = 3600;
+
 export default async (request: ServerRequest) => {
   const body = JSON.stringify(await zennApi(request.url));
-  const headers = new Headers({ "Content-Type": "application/json" });
-  // console.log(body);
+  const headers = new Headers({
+    "Cache-Control": `public, max-age=${CACHE_MAX_AGE}`,
+    "Content-Type": "application/json",
+  });
   request.respond({ status: 200, headers, body });
 };
