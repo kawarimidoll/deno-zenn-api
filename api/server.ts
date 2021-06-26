658db6373a24d8987767fa66bd1da488de2634cd
kawarimidoll <kawarimidoll+git@gmail.com>
2021-06-26 21:44:07 +0900

    fix to use native http api

--- api/server.ts
+++ api/server.ts
@@ -1,14 +1,20 @@
-import { ServerRequest } from "./deps.ts";
 import { zennApi } from "./zenn_api.ts";
 
 // cache one hour
 const CACHE_MAX_AGE = 3600;
 
-export default async (request: ServerRequest) => {
-  const body = JSON.stringify(await zennApi(request.url));
-  const headers = new Headers({
-    "Cache-Control": `public, max-age=${CACHE_MAX_AGE}`,
-    "Content-Type": "application/json",
-  });
-  request.respond({ status: 200, headers, body });
+export default async (request: Deno.Conn) => {
+  const httpCon = Deno.serveHttp(request);
+  for await (const event of httpCon) {
+    const { url } = event.request;
+    const { pathname } = new URL(url);
+    const body = JSON.stringify(await zennApi(pathname));
+
+    const headers = new Headers({
+      "Cache-Control": `public, max-age=${CACHE_MAX_AGE}`,
+      "Content-Type": "application/json",
+    });
+
+    event.respondWith(new Response(body, { status: 200, headers }));
+  }
 };
--- debug.ts
+++ debug.ts
@@ -1,8 +1,7 @@
-import { serve } from "./api/deps.ts";
 import handler from "./api/server.ts";
 
 const port = 8080;
-const server = serve({ port });
+const server = Deno.listen({ port });
 console.log(`HTTP webserver running. Access it at: http://localhost:${port}/`);
 
 for await (const request of server) {
