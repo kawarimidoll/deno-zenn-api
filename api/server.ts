557d4eb4bcad44a4436297cde11f2307acd1b536
kawarimidoll <kawarimidoll+git@gmail.com>
2021-10-23 13:57:15 +0900

    update std/server

--- api/server.ts
+++ api/server.ts
@@ -1,14 +1,18 @@
-import { ServerRequest } from "./deps.ts";
+import { listenAndServe } from "./deps.ts";
 import { zennApi } from "./zenn_api.ts";
 
 // cache one hour
 const CACHE_MAX_AGE = 3600;
 
-export default async (request: ServerRequest) => {
-  const body = JSON.stringify(await zennApi(request.url));
+const addr = ":8000";
+console.log(`server running at http://localhost${addr}`);
+
+listenAndServe(addr, async (request) => {
+  const url = new URL(request.url);
+  const body = JSON.stringify(await zennApi(url.pathname));
   const headers = new Headers({
     "Cache-Control": `public, max-age=${CACHE_MAX_AGE}`,
     "Content-Type": "application/json",
   });
-  request.respond({ status: 200, headers, body });
-};
+  return new Response(body, { status: 200, headers });
+});
