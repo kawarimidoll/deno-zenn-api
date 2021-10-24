353dba6f4a4731ccabcb44415e9f96f6b9b8c08f
kawarimidoll <kawarimidoll+git@gmail.com>
2021-10-24 16:12:30 +0900

    chore(deps): remove unused imports

--- deps.ts
+++ deps.ts
@@ -1,9 +1,7 @@
 import ky, { SearchParamsOption } from "https://cdn.skypack.dev/ky?dts";
 import { DOMParser } from "https://deno.land/x/deno_dom@v0.1.15-alpha/deno-dom-wasm.ts";
 
-import { listenAndServe } from "https://deno.land/std@0.112.0/http/server.ts";
-
 import { assert } from "https://deno.land/std@0.112.0/testing/asserts.ts";
 
-export { assert, DOMParser, ky, listenAndServe };
+export { assert, DOMParser, ky };
 export type { SearchParamsOption };
