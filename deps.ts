911b894520daf3e302a05350597d177870f7dac1
kawarimidoll <kawarimidoll+git@gmail.com>
2022-08-28 20:03:09 +0900

    chore(deps): update deno dependencies

--- deps.ts
+++ deps.ts
@@ -1,7 +1,7 @@
 import ky, { SearchParamsOption } from "https://cdn.skypack.dev/ky?dts";
-import { DOMParser } from "https://deno.land/x/deno_dom@v0.1.15-alpha/deno-dom-wasm.ts";
+import { DOMParser } from "https://deno.land/x/deno_dom@v0.1.33-alpha/deno-dom-wasm.ts";
 
-import { assert } from "https://deno.land/std@0.112.0/testing/asserts.ts";
+import { assert } from "https://deno.land/std@0.152.0/testing/asserts.ts";
 
 export { assert, DOMParser, ky };
 export type { SearchParamsOption };
